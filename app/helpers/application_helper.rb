2ad9dddb149613d7c24f8bda1cadb0b8c1cc5827
Maciej Sypniewski <msypniewski511@gmail.com>
2022-03-01 09:56:57 +0000

    Add TOC to mardown and change layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,29 +4,95 @@ module ApplicationHelper
   def has_role?(role)
     current_user && current_user.has_role?(role)
   end
+
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      language ||= 'ruby'
+      language ||= 'markdown'
       CodeRay.scan(code, language).div
     end
   end
 
-  def markdown(text)
-    coderayified = CodeRayify.new(:hard_wrap => true)
+  def markdown_1(text)
+    coderayified = CodeRayify.new(:hard_wrap => true, with_toc_data: true)
     options = {
       fenced_code_blocks: true,
       no_intra_emphasis: true,
       autolink: true,
       lax_html_blocks: true,
       lax_spacing: true,
-      tables: true
+      tables: true,
+      with_toc_data: true
     }
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    
     markdown_to_html.render(text).html_safe
   end
 
   def strip_markdown(text)
     markdown_to_plain_text = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
     markdown_to_plain_text.render(text).html_safe
+  end
+
+
+  def markdown(text)
+    # output = ["<div class='toc_text'>"]
+    # content = text
+    # unsafe_chars_regex = /[ $&+,\/:;=?@\"<>#%{}|\^~\[\]`]+/o
+
+    # # Render the Table of Contents
+    # renderer = Redcarpet::Render::HTML_TOC
+    # toc_text = Redcarpet::Markdown.new(renderer).render(content)
+
+    # # Fix redcarpet anchor links; see bug
+    # # https://github.com/vmg/redcarpet/issues/385
+    # toc_text.gsub!(/<a href="(.*)#(.*)">/) do |match|
+    #   link_url = $1
+    #   fixed_anchor = CGI.unescapeHTML($2).gsub(unsafe_chars_regex, '')
+
+    #   "<a href=\"#{link_url}##{fixed_anchor}\">"
+    # end
+    # # tmp_toc_wrap = "<div class='toc_text'>"
+    # output << toc_text << "</div>"
+
+    # # Then render the content
+    # renderer = Redcarpet::Render::HTML.new(with_toc_data: true)
+    # body_text = Redcarpet::Markdown.new(renderer).render(content)
+
+    # # Fix redcarpet header IDs user for anchors; see bug
+    # # https://github.com/vmg/redcarpet/issues/385
+    # body_text.gsub!(/<h([1-6]) id="(.*)"/) do |match|
+    #   header_level = $1
+    #   fixed_anchor = CGI.unescapeHTML($2).gsub!(unsafe_chars_regex, '')
+
+    #   "<h#{header_level} id=\"#{fixed_anchor}\""
+    # end
+
+
+    output = []
+    output << markdown_1(text)
+
+    output.join('').html_safe
+  end
+
+  def get_toc(text)
+    output = ["<div class='toc_text'>", "<h3 class='title is-5 has-text-grey-light discussion-title'>Table of Content</h3>"]
+    content = text
+    unsafe_chars_regex = /[ $&+,\/:;=?@\"<>#%{}|\^~\[\]`]+/o
+
+    # Render the Table of Contents
+    renderer = Redcarpet::Render::HTML_TOC
+    toc_text = Redcarpet::Markdown.new(renderer).render(content)
+
+    # Fix redcarpet anchor links; see bug
+    # https://github.com/vmg/redcarpet/issues/385
+    toc_text.gsub!(/<a href="(.*)#(.*)">/) do |match|
+      link_url = $1
+      fixed_anchor = CGI.unescapeHTML($2).gsub(unsafe_chars_regex, '')
+
+      "<a href=\"#{link_url}##{fixed_anchor}\">"
+    end
+    # tmp_toc_wrap = "<div class='toc_text'>"
+    output << toc_text << "</div>"
+    output.join('').html_safe
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  
+  root 'discussions#index'
   resources :channels
   resources :discussions do
     resources :replies
   end
-
-  root 'discussions#index'
   devise_for :users, controllers: { registrations: 'registrations' }
 end
