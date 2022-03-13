db285290fe30bf937e8922e88bb9dddc1695fbe3
Maciej Sypniewski <msypniewski511@gmail.com>
2022-03-13 16:53:12 +0000

    Add validation to attachments on discussion model

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -8,4 +8,15 @@ class Discussion < ApplicationRecord
   resourcify
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validate :main_picture_format
+
+  private
+
+  def main_picture_format
+    return unless main_picture.attached?
+    return if main_picture.blob.content_type.start_with? 'image/'
+    main_picture.purge_later
+    errors.add(:main_picture, 'needs to be an image')
+  end
 end
