264f84c445ea824aed57e1e4edf212fe88f26027
Maciej <msypniewski511@gmail.com>
2022-11-20 03:43:50 +0000

    sadvf

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Knowledge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
+    config.secret_key_base = 'blipblapblup'
 
     # Configuration for the application, engines, and railties goes here.
     #
