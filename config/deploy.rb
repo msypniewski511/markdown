53b438e5dc30757fd33186e32c3594773a958b92
Maciej <msypniewski511@gmail.com>
2022-11-20 01:55:51 +0000

    Add capistrano to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.1"
 
 set :application, "knowladgebase"
-set :repo_url, "git@github.com:msypniewski511/knowladgebase.git"
+set :repo_url, "https://github.com/msypniewski511/knowladgebase"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deploy/#{fetch :aplication}"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
